6a2676e53f856b0a5ed2c4fd849f68da9f2b4446
Kasey Schindler <kasey.schindler@me.com>
2012-03-06 18:47:46 -0600

    Added files

---
+++ KS-SearchViewController/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  KS-SearchViewController
+//
+//  Created by Kasey Schindler on 2/27/12.
+//  Copyright (c) 2012 Kasey Schindler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
